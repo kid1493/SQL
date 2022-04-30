7a42dd845aafd1689068a91d90c7be0a1f96b022
kid1493 <34921641+kid1493@users.noreply.github.com>
2022-04-30 09:41:57 +0900

    Create CalcNull.sql

---
+++ CalcNull.sql
@@ -0,0 +1,10 @@
+#입양 게시판에 동물 정보를 게시하려 합니다. 동물의 생물 종, 이름, 성별 및 중성화 여부를 아이디 순으로 조회하는 SQL문을 작성해주세요.
+
+#이때 프로그래밍을 모르는 사람들은 NULL이라는 기호를 모르기 때문에, 
+
+#이름이 없는 동물의 이름은 "No name"으로 표시해 주세요.
+
+
+select animal_type , ifnull(name ,'No name') , sex_upon_intake
+from animal_ins
+order by animal_id
